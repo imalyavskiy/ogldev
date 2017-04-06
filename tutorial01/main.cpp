acabc0b08f21167d73f840f375428608af86f409
ilya malyavskiy <ilya.malyavskiy@gmail.com>
2017-04-06 23:13:04 +0700

    Разобрал два примера, снабдил из коментариями.

--- tutorial01/main.cpp
+++ tutorial01/main.cpp
@@ -20,31 +20,52 @@
 
 #include <GL/freeglut.h>
 
-
+// Функция заполнения буфера кадра
 static void RenderSceneCB()
 {
+	// Очищаем буфер кадра
     glClear(GL_COLOR_BUFFER_BIT);
+	
+	/*
+	TODO: Производим отрисовку
+	*/
+	
+	// меняем местами буфера кадров(у нас их два, мы включали двойную буфферизацию).
     glutSwapBuffers();
 }
 
+// Функция регистрации callback'ов
 static void InitializeGlutCallbacks()
 {
+	// Устанавливаем callback функцию заполнения буфера кадра
     glutDisplayFunc(RenderSceneCB);
 }
 
-
+// Собственно точка входа
 int main(int argc, char** argv)
 {
+	// Инициализация GLUT
     glutInit(&argc, argv);
+	
+	// Включаем режим двойной буферизации(GLUT_DOUBLE) и режим вывода цветов(GLUT_RGBA, не индексированный режим цветопередачи).
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+	
+	// Задаём размер окна
     glutInitWindowSize(1024, 768);
+	
+	// Задаём позицию верхнего левого угла нашего окна
     glutInitWindowPosition(100, 100);
+	
+	// Создаём окно с указанным именем
     glutCreateWindow("Tutorial 01");
 
+	// Регистрируем callback'и
     InitializeGlutCallbacks();
 
+	// Задаём цвет заливки буфера кадра
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+	// Передаём управление главному циклу GLUT
     glutMainLoop();
     
     return 0;
