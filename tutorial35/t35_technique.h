2e4baac0c463d84f288f24f029e7774ad099ca60
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2024-12-15 23:43:58 +0700

    lessons made buildable, in t35 glfx replaced with raw impl fro original lessons, t36 and t37 are not working properly

---
+++ tutorial35/t35_technique.h
@@ -0,0 +1,91 @@
+/*
+
+	Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TECHNIQUE_H
+#define	TECHNIQUE_H
+
+#include <list>
+#include <GL/glew.h>
+
+namespace t35
+{
+  namespace glfx
+  {
+    class Technique
+    {
+    public:
+
+      Technique(const char* pEffectFile);
+
+      ~Technique();
+
+      void Enable();
+
+    protected:
+
+      bool CompileProgram(const char* pProgram);
+
+      GLint GetUniformLocation(const char* pUniformName);
+
+      GLint GetProgramParam(GLint param);
+
+    private:
+      GLint m_effect;
+      GLint m_shaderProg;
+      const char* m_pEffectFile;
+    };
+  }
+
+  namespace bare
+  {
+    class Technique
+    {
+    public:
+
+      Technique();
+
+      virtual ~Technique();
+
+      virtual bool Init();
+
+      void Enable();
+
+      GLuint GetProgram() const { return m_shaderProg; }
+
+    protected:
+
+      bool AddShader(GLenum ShaderType, const char* pFilename);
+
+      bool Finalize();
+
+      GLint GetUniformLocation(const char* pUniformName);
+
+      GLuint m_shaderProg = 0;
+
+    private:
+
+      void PrintUniformList();
+
+      typedef std::list<GLuint> ShaderObjList;
+      ShaderObjList m_shaderObjList;
+    };
+  }
+}
+
+#endif	/* TECHNIQUE_H */
+
---
+++ tutorial37/t37_texture.cpp
@@ -0,0 +1,107 @@
+/*
+
+    Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cassert>
+#include <iostream>
+#include "t37_texture.h"
+
+namespace t37
+{
+  FIBITMAP* GenericLoader(const char* lpszPathName, int flag) {
+    auto fif = FIF_UNKNOWN;
+
+    fif = FreeImage_GetFileType(lpszPathName, 0);
+    if (fif == FIF_UNKNOWN) {
+      fif = FreeImage_GetFIFFromFilename(lpszPathName);
+    }
+
+    if ((fif != FIF_UNKNOWN) && FreeImage_FIFSupportsReading(fif)) {
+      return FreeImage_Load(fif, lpszPathName, flag);
+    }
+
+    return nullptr;
+  }
+
+  bool GenericWriter(FIBITMAP* dib, const std::string& lpszPathName, int flag) {
+    FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+    BOOL bSuccess = FALSE;
+
+    if (dib) {
+      fif = FreeImage_GetFIFFromFilename(lpszPathName.c_str());
+      if (fif != FIF_UNKNOWN) {
+        WORD bpp = FreeImage_GetBPP(dib);
+        if (FreeImage_FIFSupportsWriting(fif) && FreeImage_FIFSupportsExportBPP(fif, bpp)) {
+          bSuccess = FreeImage_Save(fif, dib, lpszPathName.c_str(), flag);
+        }
+      }
+    }
+    return (bSuccess == TRUE) ? true : false;
+  }
+
+  Texture::Texture(GLenum TextureTarget, std::string FileName)
+    : m_fileName(std::move(FileName))
+    , m_textureTarget(TextureTarget)
+  {
+    glGenTextures(1, &m_textureObj);
+  }
+
+  bool Texture::Load() const
+  {
+    auto src = GenericLoader(m_fileName.c_str(), 0);
+    if (!src)
+      return false;
+
+    const auto type = FreeImage_GetColorType(src);
+    if (!(type == FIC_RGBALPHA || type == FIC_RGB))
+      return false;
+
+    if (!FreeImage_HasPixels(src))
+      return false;
+
+    const auto bpp = FreeImage_GetBPP(src);
+    assert((type == FIC_RGBALPHA && 32 == bpp) || (type == FIC_RGB && 24 == bpp));
+
+    const GLsizei width = FreeImage_GetWidth(src);
+    const GLsizei height = FreeImage_GetHeight(src);
+
+    FreeImage_FlipVertical(src);
+
+    const auto data = FreeImage_GetBits(src);
+
+    glBindTexture(m_textureTarget, m_textureObj);
+    if (type == FIC_RGBALPHA && 32 == bpp)
+      glTexImage2D(m_textureTarget, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, data);
+    else if (type == FIC_RGB && 24 == bpp)
+      glTexImage2D(m_textureTarget, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+    else
+      throw std::logic_error("Unsupported color format");
+
+    glTexParameterf(m_textureTarget, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(m_textureTarget, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    FreeImage_Unload(src);
+
+    return true;
+  }
+
+  void Texture::Bind(const GLenum TextureUnit) const
+  {
+    glActiveTexture(TextureUnit);
+    glBindTexture(m_textureTarget, m_textureObj);
+  }
+}
